0640fe87892934fd2b728b5662a5745db43bbe20
Platanus <Platanus.carl@gmail.com>
2019-03-24 14:22:00 +0800

    for a test

---
+++ ex01.py
@@ -0,0 +1,7 @@
+print ('now name the "X": ', end='')
+x = int(input())
+print ('and then the "y": ', end='')
+y = int(input())
+z = x + y
+
+print (z)
---
+++ ex02.py
@@ -0,0 +1,6 @@
+age = input("age? ")
+name = input("name? ")
+height = input ("height? ")
+
+print ("so, you are {},age {},and height{}.".format(name,age,height))
+print (f"{age} {name} {height}" )
